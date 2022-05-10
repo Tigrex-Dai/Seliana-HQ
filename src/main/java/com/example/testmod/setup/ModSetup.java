c2e8703823bf9830b430fb7ad70516ad438e6d81
TigrexDai <wsgjsandrpk@foxmail.com>
2022-05-10 17:20:35 +0200

    Going on to 1st 36:08

--- src/main/java/com/example/testmod/setup/ModSetup.java
+++ src/main/java/com/example/testmod/setup/ModSetup.java
@@ -1,8 +1,19 @@
 package com.example.testmod.setup;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 public class ModSetup {
+
+    public static final String TAB_NAME = "testmod";
+
+    public static final CreativeModeTab ITEM_GROUP = new CreativeModeTab(TAB_NAME) {
+        @Override
+        public ItemStack makeIcon() { return new ItemStack(Items.NETHERITE_INGOT); }
+    };
+
     public static void init(FMLCommonSetupEvent event){
 
     }
--- src/main/java/com/example/testmod/setup/Registration.java
+++ src/main/java/com/example/testmod/setup/Registration.java
@@ -1,11 +1,15 @@
 package com.example.testmod.setup;
 
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 import static com.example.testmod.TestMod.MODID;
 
@@ -20,5 +24,16 @@ public class Registration {
         BLOCKS.register(bus);
         ITEMS.register(bus);
 
+    }
+
+    // properties for blocks and items
+    public static final BlockBehaviour.Properties ORE_PROPERTIES = BlockBehaviour.Properties.of(Material.STONE).strength(2f);
+    public static final Item.Properties ITEM_PROPERTIES = new Item.Properties().tab(ModSetup.ITEM_GROUP);
+
+    public static final RegistryObject<Block> XXXXXXX = BLOCKS.register("XXXXXXX", () -> new Block(ORE_PROPERTIES));
+    public static final RegistryObject<Item> XXXXXXX_ITEM = fromBlock(XXXXXXX);
+
+    public static <B extends Block> RegistryObject<Item> fromBlock(RegistryObject<B> block){
+        return ITEMS.register(block.getId().getPath(), () -> new BlockItem(block.get(), ITEM_PROPERTIES));
     }
 }
